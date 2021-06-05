17434f5cd7b1a525ff1d988301e0390b17f5b411
KenBusch <ken0505@gmail.com>
2021-06-05 18:23:50 -0500

    change 7

---
+++ netlify/functions/messages.js
@@ -0,0 +1,49 @@
+// Goal: Provide a function to return all posts and their comments from Firebase.
+
+
+//let url = `/.netlify/functions/messages?chatroomname=${chatroomName}`
+
+// allows us to use firebase
+let firebase = require(`./firebase`)
+// /.netlify/functions/posts
+exports.handler = async function(event) {
+  // define an empty Array to hold the return value from our lambda
+  let returnValue = []
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  //Get the chatroom name from the querystring parameter
+  let chatroomName = event.queryStringParameters.chatroomname
+
+  console.log(chatroomName)
+
+  // perform a query against firestore for all posts, wait for it to return, store in memory
+  let messagesQuery = await db.collection(`messages`).where('chatroom', '==', chatroomName).get()
+
+  // retrieve the documents from the query
+  let messages = messagesQuery.docs
+
+  // loop through the post documents
+  for (let i=0; i < messages.length; i++) {
+    // get the id from the document
+    let messageId = messages[i].id
+    // get the data from the document
+    let messageData = messages[i].data()
+    // create an Object to be added to the return value of our lambda
+    let messagesObject = {
+      id: messageId,
+      body: messageData.body,
+      chatroom: messageData.chatroom,
+      time: messageData.time,
+      userName: messageData.userName
+    }
+    // add the Object to the return value
+    returnValue.push(messagesObject)
+  }
+
+  // return value of our lambda
+  return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+  }
+}
