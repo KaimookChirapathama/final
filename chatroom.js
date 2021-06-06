019a2432ebc16b637377af1a5a3817b487bd4580
Kaimook <kaimook.chirapathama@kellogg.northwestern.edu>
2021-06-05 22:50:47 -0500

    change 12

--- chatroom.js
+++ chatroom.js
@@ -23,9 +23,9 @@ firebase.auth().onAuthStateChanged(async function(user) {
   let titleDiv = document.querySelector(`.roomTitle`)
         //insert HTML for submission form into page for signed in user 
           titleDiv.insertAdjacentHTML(`beforeend`, `
-          <div class="py-8 px-8 md:px-8 py-8 md:w-1/2 w-full mx-auto text-3xl text-center text-purple-500">
+          <div class="py-8 px-8 md:px-8 py-8 md:w-1/2 w-full mx-auto text-center text-purple-500">
     
-    <div class="md:mx-0 mx-4"><span class="font-bold text-2xl bg-clip-text">${chatroomName}</span></div>
+    <div class="md:mx-0 mx-4"><span class="font-bold text-4xl bg-clip-text">${chatroomName}</span></div>
             `)
   // 🔥🔥🔥Populate chatroom title from query string parameters🔥🔥🔥
 
