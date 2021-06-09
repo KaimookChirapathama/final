1ea975f1e3c98e6959d0e71a96290f8da8effbab
Kelsey Anderson <kelsey.anderson@kellogg.northwestern.edu>
2021-06-08 23:04:06 -0500

    chatroom updates

--- chatroom.js
+++ chatroom.js
@@ -28,6 +28,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
           <div class="py-8 px-8 md:px-8 py-8 md:w-1/2 w-full mx-auto text-center text-blue-500">
     
     <div class="md:mx-0 mx-4"><span class="font-bold text-4xl bg-clip-text">${chatroomName} Chatroom </span></div>
+    <div class="md:mx-0 mx-4"><span class="font-light text-1xl bg-clip-text">Join this conversation by typing in your comment below.</div>
             `)
   // 🔥🔥🔥Populate chatroom title from query string parameters🔥🔥🔥
 
